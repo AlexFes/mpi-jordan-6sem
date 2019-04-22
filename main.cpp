83e57b60544804bbc5ccadfe4169c8015009865d
AlexFes <alexeiruxur@gmail.com>
2019-04-22 21:27:12 +0300

    first attempt

--- main.cpp
+++ main.cpp
@@ -8,19 +8,54 @@
 int main (int argc, char* argv[]) {
     MPI_Init (&argc, &argv);
 
-    int r, np, n = atoi(argv[1]);
+    int r;
+    int np;
+    int n = atoi(argv[1]);
 
     MPI_Comm_rank (MPI_COMM_WORLD, &r);
     MPI_Comm_size (MPI_COMM_WORLD, &np);
 
     double* a = new double[(n/np + 1)*n];
     double* b = new double[n/np + 1];
+    double* b1 = new double[n/np + 1];
+    double res;
+    double time;
+    char* input = NULL;
+    if (argc == 3) {
+        input = argv[2];
+    }
 
-    createMatrix(n, r, np, a, b);
+    for (int i = 0; i < n/np + 1; ++i) {
+        b[i] = 0;
+        b1[i] = 0;
+    }
+
+    if (argc == 3) {
+        readMatrix(n, r, np, a, b, input);
+    } else {
+        createMatrix(n, r, np, a, b);
+    }
     writeMatrix(n, r, np, a, b);
 
+    time = MPI_Wtime();
     solveMatrix(n, r, np, a, b);
-    writeMatrix(n, r, np, a, b);
+    time = MPI_Wtime() - time;
+
+    if (argc == 3) {
+        readMatrix(n, r, np, a, b1, input);
+    } else {
+        createMatrix(n, r, np, a, b1);
+    }
+    res = discrepancy(n, r, np, a, b1, b);
+
+    if (r == 0) {
+        printf("\n res=%e \n", res);
+        printf("\n time=%.2f \n", time);
+    }
+
+    delete[]a;
+    delete[]b;
+    delete[]b1;
 
     MPI_Finalize();
     return 0;
--- methods.cpp
+++ methods.cpp
@@ -4,25 +4,62 @@
 
 using namespace std;
 
-void createMatrix (int n, int r, int np, double *a, double *b);
-void writeMatrix (int n, int r, int np, double *a, double *b);
+void readMatrix(int n, int r, int np, double *a, double *b, char* input);
+void createMatrix(int n, int r, int np, double *a, double *b);
+void writeMatrix(int n, int r, int np, double *a, double *b);
+void solveMatrix(int n, int r, int np, double *a, double *b);
+double discrepancy(int n, int r, int np, double *a, double *b, double* answer);
+
+// Read matrix from file
+void readMatrix(int n, int r, int np, double *a, double *b, char* input) {
+    MPI_Status st;
+    FILE *in = fopen(input, "r");
+    double* buf = new double[n + 1];
+
+    for (int iGlobal = 0; iGlobal < n; ++iGlobal) {
+        if (r == 0) {
+            for (int i = 0; i < n + 1; ++i) {
+                fscanf(in, "%lf", &buf[i]);
+            }
+
+            if (r == iGlobal%np) {
+                memcpy(a + (iGlobal/np)*n, buf, n*sizeof(double));
+                b[iGlobal/np] = buf[n];
+            } else {
+                MPI_Send(buf, n + 1, MPI_DOUBLE, iGlobal%np, 0, MPI_COMM_WORLD);
+            }
+        } else if (r != 0 && r == iGlobal%np) {
+            MPI_Recv(buf, n + 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+            memcpy(a + (iGlobal/np)*n, buf, n*sizeof(double));
+            b[iGlobal/np] = buf[n];
+        }
+    }
+
+    delete[]buf;
+    fclose(in);
+
+    return;
+}
 
 // Generate matrix shape n x n for np processes
-void createMatrix (int n, int r, int np, double *a, double *b) {
-    for (int i = 0; i < n/np + (r + 1 <= n%np ? 1 : 0); ++i) {
+void createMatrix(int n, int r, int np, double *a, double *b) {
+    int nLocal = n/np + (r + 1 <= n%np ? 1 : 0);
+
+    for (int i = 0; i < nLocal; ++i) {
         b[i] = 0;
 
         for (int j = 0; j < n; ++j) {
-            // a[i*n + j] = max(i*np + r, j);
-            // a[i*n + j] = 1.0/(i*np + r + j);
-            a[i*n + j] = abs(i*np + r - j);
+            a[i*n + j] = max(i*np + r, j);
+            // a[i*n + j] = abs(i*np + r - j);
             b[i] += j%2 ? 0 : a[i*n + j];
         }
     }
+
+    return;
 }
 
 // Output matrix to stdout
-void writeMatrix (int n, int r, int np, double *a, double *b) {
+void writeMatrix(int n, int r, int np, double *a, double *b) {
     MPI_Status st;
     int N = min(n, 10);
     double* buf = new double[n + 1];
@@ -61,8 +98,94 @@ void writeMatrix (int n, int r, int np, double *a, double *b) {
     return;
 }
 
+double discrepancy(int n, int r, int np, double* a, double* b, double* answer) {
+    // Number of rows
+    int nLocal = n/np + (r + 1 <= n%np ? 1 : 0);
+
+    double tmp = 0;
+
+    double* answerBuf = new double[(n/np + 1)*np];
+    double* fullAnswer = new double[n];
+
+    double* resultBuf = new double[n/np + 1];
+    double* fullResultBuf = new double[(n/np + 1)*np];
+    double* fullResult = new double[n];
+
+    // Fill the rest of the answer
+    if (nLocal < n/np + 1) {
+        answer[nLocal] = 0;
+    }
+
+    for (int i = 0; i < n/np + 1; ++i) {
+        resultBuf[i] = 0;
+    }
+
+    // Gather the answer vectors
+    MPI_Allgather(answer, n/np + 1, MPI_DOUBLE, answerBuf, n/np + 1, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    // Build the full answer out of answerBuf
+    for (int tmpRank = 0; tmpRank < np; ++tmpRank) {
+        int tmpMaxRow = n/np + (tmpRank + 1 <= n%np ? 1 : 0);
+
+        for (int iLocal = 0; iLocal < tmpMaxRow; ++iLocal) {
+            fullAnswer[np*iLocal + tmpRank] = answerBuf[iLocal + tmpRank*(n/np + 1)];
+        }
+    }
+
+    // Multiply matrix on vector
+    for (int iLocal = 0; iLocal < nLocal; ++iLocal) {
+        for (int j = 0; j < n; ++j) {
+            resultBuf[iLocal] += a[iLocal*n + j]*fullAnswer[j];
+        }
+    }
+
+    // Send result to the root process
+    MPI_Gather(resultBuf, n/np + 1, MPI_DOUBLE, fullResultBuf, n/np + 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Build the full result out of fullResultBuf
+    if (r == 0) {
+        for (int tmpRank = 0; tmpRank < np; ++tmpRank) {
+            int tmpMaxRow = n/np + (tmpRank + 1 <= n%np ? 1 : 0);
+
+            for (int iLocal = 0; iLocal < tmpMaxRow; ++iLocal) {
+                fullResult[np*iLocal + tmpRank] = fullResultBuf[iLocal + tmpRank*(n/np + 1)];
+            }
+        }
+    }
+
+    // Send vector b to the root process
+    MPI_Gather(b, n/np + 1, MPI_DOUBLE, answerBuf, n/np + 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (r == 0) {
+        for (int tmpRank = 0; tmpRank < np; ++tmpRank) {
+            int tmpMaxRow = n/np + (tmpRank + 1 <= n%np ? 1 : 0);
+
+            for (int iLocal = 0; iLocal < tmpMaxRow; ++iLocal) {
+                fullAnswer[np*iLocal + tmpRank] = answerBuf[iLocal + tmpRank*(n/np + 1)];
+            }
+        }
+    }
+
+    // Evaluate discrepancy
+    if (r == 0) {
+        for (int i = 0; i < n; ++i) {
+            if (fabs(fullResult[i] - fullAnswer[i]) > tmp) {
+                tmp = fabs(fullResult[i] - fullAnswer[i]);
+            }
+        }
+    }
+
+    delete[]answerBuf;
+    delete[]fullAnswer;
+    delete[]resultBuf;
+    delete[]fullResultBuf;
+    delete[]fullResult;
+
+    return (r == 0 ? tmp : -1);
+}
+
 // Solve system of linear equations (Gauss–Jordan elimination); array b holds the result vector
-void solveMatrix (int n, int r, int np, double* a, double* b) {
+void solveMatrix(int n, int r, int np, double* a, double* b) {
     // Number of rows; each process has matrix shape nLocal x n
     int nLocal = n/np + (r + 1 <= n%np ? 1 : 0);
 
@@ -75,8 +198,9 @@ void solveMatrix (int n, int r, int np, double* a, double* b) {
     for (int iGlobal = 0; iGlobal < n; ++iGlobal) {
         int firstRow = iGlobal%np <= r ? iGlobal/np : iGlobal/np + 1;
         double tmp = fabs(firstRow >= nLocal ? 0 : a[firstRow*n + iGlobal]);
-        int tmpRow = firstRow >= nLocal ? -1 : firstRow;
+        int tmpRow = firstRow;
         int tmpRank = r;
+        int permutationTmp = 0;
 
         // Get main element for process
         for (int iLocal = firstRow + 1; iLocal < nLocal; ++iLocal) {
@@ -93,7 +217,7 @@ void solveMatrix (int n, int r, int np, double* a, double* b) {
         for (int i = 0; i < 2*np; i+=2) {
             if (fabs(mainElement[i]) > tmp || (fabs(mainElement[i]) >= tmp && fabs(mainElement[i]) <= tmp && tmpRank > i/2)) {
                 tmp = fabs(mainElement[i]);
-                tmpRow = mainElement[i + 1];
+                tmpRow = (int) mainElement[i + 1];
                 tmpRank = i/2;
             }
         }
@@ -129,5 +253,45 @@ void solveMatrix (int n, int r, int np, double* a, double* b) {
             memcpy(a + (iGlobal/np)*n + iGlobal, buf, (n - iGlobal)*sizeof(double));
             b[iGlobal/np] = buf[n - iGlobal];
         }
+
+        // Subtract main row from all others
+        if (r == iGlobal%np) {
+            for (int iLocal = 0; iLocal < iGlobal/np; ++iLocal) {
+                tmp = a[iLocal*n + iGlobal]/buf[0];
+                b[iLocal] = b[iLocal] - tmp*buf[n - iGlobal];
+
+                for (int j = iGlobal + 1; j < n; ++j) {
+                    a[iLocal*n + j] = a[iLocal*n + j] - tmp*buf[j - iGlobal];
+                }
+            }
+            for (int iLocal = iGlobal/np + 1; iLocal < nLocal; ++iLocal) {
+                tmp = a[iLocal*n + iGlobal]/buf[0];
+                b[iLocal] = b[iLocal] - tmp*buf[n - iGlobal];
+
+                for (int j = iGlobal + 1; j < n; ++j) {
+                    a[iLocal*n + j] = a[iLocal*n + j] - tmp*buf[j - iGlobal];
+                }
+            }
+            for (int j = iGlobal + 1; j < n; ++j) {
+                a[(iGlobal/np)*n + j] /= a[(iGlobal/np)*n + iGlobal];
+            }
+
+            b[iGlobal/np] /= a[(iGlobal/np)*n + iGlobal];
+        } else {
+            for (int iLocal = 0; iLocal < nLocal; ++iLocal) {
+                tmp = a[iLocal*n + iGlobal]/buf[0];
+                b[iLocal] = b[iLocal] - tmp*buf[n - iGlobal];
+
+                for (int j = iGlobal + 1; j < n; ++j) {
+                    a[iLocal*n + j] = a[iLocal*n + j] - tmp*buf[j - iGlobal];
+                }
+            }
+        }
     }
+
+    delete[]mainElement;
+    delete[]buf;
+    delete[]buf1;
+
+    return;
 }
